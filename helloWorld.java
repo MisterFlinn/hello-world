6810f69cc2ac2f185c36436e0af887358fcde2de
mel-pina <134322790+mel-pina@users.noreply.github.com>
2023-05-24 10:59:38 -0700

    Update helloWorld.java
    update it to the epic pokemon

--- helloWorld.java
+++ helloWorld.java
@@ -3,6 +3,6 @@ public class helloWorld
     public static void main(String[] args)
     {
        System.out.println("Hello World");
-       System.out.println("This is a test");
+       System.out.println("Whimsicott");
     } 
 }
